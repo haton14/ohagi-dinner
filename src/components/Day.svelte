ae7c102886281645a13941fb8797a3717df776aa
haton14 <iioka.takumi.quena@gmail.com>
2021-12-20 16:49:03 +0900

    日本語曜日をdayjsで取得する

--- src/components/Day.svelte
+++ src/components/Day.svelte
@@ -1,4 +1,15 @@
+<script lang="ts">
+  import dayjs from "dayjs";
+  import { weekdaysShort as weekdays } from "dayjs/locale/ja";
+  
+  export let year: number;
+  export let month: number;
+  export let day: number;
+
+  let weekday: string = weekdays[dayjs(`${year}-${month}-${day}`).day()];
+</script>
+
 <div class="p-2 justify-between">
-  <p>金</p>
-  <p>1</p>
+  <p>{weekday}</p>
+  <p>{day}</p>
 </div>
