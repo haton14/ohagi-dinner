8d8c1c803bbbc449bbde483158ea4fea8e9b7ae7
haton14 <iioka.takumi.quena@gmail.com>
2021-12-20 16:49:16 +0900

    年月表示Componet作成

---
+++ src/components/YearMonth.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    export let year: number;
+    export let month: number;
+  </script>
+  
+  <h1>{year}年{month}月</h1>
--- src/pages/Records.svelte
+++ src/pages/Records.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
   import Record from "../components/Record.svelte";
+  import YearMonth from "../components/YearMonth.svelte";
 </script>
 
 <div>
-  <h1>Top</h1>
+  <YearMonth year={2021} month={12} />
   <div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
     <Record year={2021} month={12} day={1} />
     <Record year={2021} month={12} day={2} />
     <Record year={2021} month={12} day={3} />
     <Record year={2021} month={12} day={4} />
+    <Record year={2021} month={12} day={31} />
+  </div>
+  <YearMonth year={2022} month={1} />
+  <div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
+    <Record year={2022} month={1} day={1} />
+    <Record year={2022} month={1} day={2} />
+    <Record year={2022} month={1} day={3} />
+    <Record year={2022} month={1} day={4} />
   </div>
 </div>
