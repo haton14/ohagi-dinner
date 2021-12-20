0a9d77be1eedbdc0d59206ee717a83a24c0ac55e
haton14 <iioka.takumi.quena@gmail.com>
2021-12-20 16:48:57 +0900

    日付Component作成

---
+++ src/components/Day.svelte
@@ -0,0 +1,4 @@
+<div class="p-2 justify-between">
+  <p>金</p>
+  <p>1</p>
+</div>
--- src/components/Record.svelte
+++ src/components/Record.svelte
@@ -1,19 +1,18 @@
 <script>
-import FoodRow from "./FoodRow.svelte";
-
+  import FoodRow from "./FoodRow.svelte";
+  import Day from "./Day.svelte";
 </script>
 
 <div class="w-full flex flex-row">
-    <div class="m-4 w-auto">
-      <div>金</div>
-      <div>1</div>
-    </div>
-    <div
-      class="border-t border-r border-b border-l border-grey-light rounded-b rounded-t p-4 flex flex-col leading-normal w-12 sm:w-5/12 sm:text-red-600 lg:w-7/12 lg:text-green-600"
-    >
-      <div class="my-4">
-        <FoodRow/>
-        <FoodRow/>
-      </div>
+  <div class="m-4 w-auto">
+    <Day />
+  </div>
+  <div
+    class="border-t border-r border-b border-l border-grey-light rounded-b rounded-t p-4 flex flex-col leading-normal w-12 sm:w-5/12 sm:text-red-600 lg:w-7/12 lg:text-green-600"
+  >
+    <div class="my-4">
+      <FoodRow />
+      <FoodRow />
     </div>
   </div>
+</div>
