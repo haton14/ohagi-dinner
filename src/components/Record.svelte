81d357d77af5c040428ad64575466e830bb27e0d
haton14 <iioka.takumi.quena@gmail.com>
2021-12-20 16:49:07 +0900

    食事記録一覧ページから日付を渡す

--- src/components/Record.svelte
+++ src/components/Record.svelte
@@ -1,11 +1,15 @@
-<script>
+<script lang="ts">
   import FoodRow from "./FoodRow.svelte";
   import Day from "./Day.svelte";
+
+  export let year: number;
+  export let month: number;
+  export let day: number;
 </script>
 
 <div class="w-full flex flex-row">
   <div class="m-4 w-auto">
-    <Day year=2021 month=12 day=2/>
+    <Day year={year} month={month} day={day} />
   </div>
   <div
     class="border-t border-r border-b border-l border-grey-light rounded-b rounded-t p-4 flex flex-col leading-normal w-12 sm:w-5/12 sm:text-red-600 lg:w-7/12 lg:text-green-600"
--- src/pages/Records.svelte
+++ src/pages/Records.svelte
@@ -1,14 +1,13 @@
-<script>
-
-import Record from "../components/Record.svelte";
-
+<script lang="ts">
+  import Record from "../components/Record.svelte";
 </script>
+
 <div>
-    <h1>Top</h1>
-    <div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
-          <Record />
-          <Record />
-          <Record />
-          <Record />
-      </div>
+  <h1>Top</h1>
+  <div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
+    <Record year={2021} month={12} day={1} />
+    <Record year={2021} month={12} day={2} />
+    <Record year={2021} month={12} day={3} />
+    <Record year={2021} month={12} day={4} />
+  </div>
 </div>
