4834c44ece05cb1b0fb00132ad85178dcaff7fb6
haton14 <iioka.takumi.quena@gmail.com>
2021-12-20 16:48:53 +0900

    食事記録一覧ページ作成

--- src/pages/Records.svelte
+++ src/pages/Records.svelte
@@ -1,3 +1,14 @@
+<script>
+
+import Record from "../components/Record.svelte";
+
+</script>
 <div>
     <h1>Top</h1>
+    <div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
+          <Record />
+          <Record />
+          <Record />
+          <Record />
+      </div>
 </div>
