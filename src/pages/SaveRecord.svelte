6f6cbbaf20b17837b938126872e64c1977a599f2
haton14 <iioka.takumi.quena@gmail.com>
2021-12-23 19:49:27 +0900

    フォームが即ふえるように修正

--- src/pages/SaveRecord.svelte
+++ src/pages/SaveRecord.svelte
@@ -4,6 +4,7 @@
   import RepositoryFactory, { RECORDS } from "../repositories/factory";
   import type { Record } from "../types/record";
   import type { Food } from "../types/food";
+  import { is_client } from "svelte/internal";
   const RecordsRepository = RepositoryFactory[RECORDS];
   let foodName: string = "";
   let amont: number = 0;
@@ -21,13 +22,13 @@
   foods.push(food);
 
   function addForm() {
-    let food2: Food = {
+    let foodAdd: Food = {
       name: "",
       amount: 0,
       unit: "",
     };
-    foods.push(food2);
-    console.log(foods.length);
+    foods.push(foodAdd);
+    formCount += 1;
   }
 
   const save = async () => {
@@ -45,26 +46,26 @@
 </script>
 
 <div>
-  {#each foods as food}
+  {#each Array(formCount) as _, i}
     <div class="p-4 flex flex-col leading-normal">
       <div class="p-2 flex flex-row justify-between">
         <input
           class="w-10/12"
-          bind:value={food.name}
+          bind:value={foods[i].name}
           type="text"
           placeholder="フード"
           required
         />
         <input
           class="w-3/12"
-          bind:value={food.amount}
+          bind:value={foods[i].amount}
           type="number"
           placeholder="量"
           required
         />
         <input
           class="w-3/12"
-          bind:value={food.unit}
+          bind:value={foods[i].unit}
           type="text"
           placeholder="単位"
           required
