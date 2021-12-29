63826f6b188c1638a2a527a10d9ab91b2a385da1
haton14 <iioka.takumi.quena@gmail.com>
2021-12-29 22:25:47 +0900

    ソート実装

--- src/pages/Records.svelte
+++ src/pages/Records.svelte
@@ -13,27 +13,27 @@
   if ($records.length < 1) {
     get();
   }
-  let before_created_at: number = 0;
-  function aa(bb: number) {
-    before_created_at = bb;
-  }
+  $records.sort(function (a, b) {
+    return a.created_at < b.created_at ? 1 : -1; //オブジェクトの昇順ソート
+  });
+
+  $: sortedRecords = $records.sort(
+    (record1, record2) => record2.created_at - record1.created_at
+  );
 </script>
 
 <div>
-    {#each $records as record, i}
-      {#if i === 0}
-        <YearMonth
-          before_created_at={0}
-          after_created_at={record.created_at}
-        />
-      {:else}
-        <YearMonth
-          before_created_at={$records[i-1].created_at}
-          after_created_at={record.created_at}
-        />
-      {/if}
-      <RecordCom created_at={record.created_at} foods={record.foods} />
-    {/each}
- 
+  {#each sortedRecords as record, i}
+    {#if i === 0}
+      <YearMonth before_created_at={0} after_created_at={record.created_at} />
+    {:else}
+      <YearMonth
+        before_created_at={$records[i - 1].created_at}
+        after_created_at={record.created_at}
+      />
+    {/if}
+    <RecordCom created_at={record.created_at} foods={record.foods} />
+  {/each}
+
   <a href={`/new`} use:link> + </a>
 </div>
