7a66fb5a0452bed6dc45146bc6db2a738f65a85e
haton14 <iioka.takumi.quena@gmail.com>
2022-02-07 22:21:26 +0900

    名前と単位をサジェストする

--- src/components/organisms/InputFood.svelte
+++ src/components/organisms/InputFood.svelte
@@ -1,12 +1,36 @@
 <script lang="ts">
   import AddButton from "../atoms/AddButton.svelte";
+  import type { Food } from "../../types/food";
+  import RepositoryFactory, { FOODS } from "../../repositories/factory";
   import { createEventDispatcher } from "svelte";
-
-  const dispatch = createEventDispatcher();
 
   export let name: string;
   export let amount: number;
   export let unit: string;
+
+  let suggestionFoods: Food[] = new Array();
+  let suggestionFoodsUniqueName: Food[] = new Array();
+  let units: string[] = new Array();
+
+  const FoodsRepository = RepositoryFactory[FOODS];
+  const get = async () => {
+    const response = await FoodsRepository.get();
+    suggestionFoods = response.foods;
+    suggestionFoodsUniqueName = uniqueName(suggestionFoods);
+  };
+  get();
+
+  const dispatch = createEventDispatcher();
+
+  function uniqueName(foods: Food[]): Food[] {
+    const names = foods.map(function (foods) {
+      return foods.name;
+    });
+    return foods.filter(function (food, index) {
+      return names.indexOf(food.name) === index;
+    });
+  }
+
   function addInputFood() {
     dispatch("addInputFood", { name: name, amount: amount, unit: unit });
   }
@@ -19,6 +43,15 @@
     type="text"
     placeholder="フード"
     required
+    list="food_name"
+    on:input={function () {
+      const f = suggestionFoods.filter(function (food) {
+        return food.name === name;
+      });
+      units = f.map(function (f) {
+        return f.unit;
+      });
+    }}
   />
   <input
     class="w-3/12"
@@ -34,8 +67,21 @@
     type="text"
     placeholder="単位"
     required
+    list="food_unit"
   />
 </div>
+
+<datalist id="food_name">
+  {#each suggestionFoodsUniqueName as food, i}
+    <option>{food.name}</option>
+  {/each}
+</datalist>
+
+<datalist id="food_unit">
+    {#each units as unit, i}
+      <option>{unit}</option>
+    {/each}
+  </datalist>
 
 <div class="p-1 flex flex-col w-9 h-9 ml-auto">
   <AddButton on:pushButton={addInputFood} />
