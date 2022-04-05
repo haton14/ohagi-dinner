2aef8acf37312c5f9d32ce40c2cf0c7471e5e072
haton14 <iioka.takumi.quena@gmail.com>
2022-04-05 22:43:08 +0900

    tsx

--- ohagi-front/app.vue
+++ ohagi-front/app.vue
@@ -1,5 +1,12 @@
-<template>
-  <div>
-    <NuxtWelcome />
-  </div>
-</template>
+<script lang="tsx">
+import HelloWorld from "./components/HelloWorld.vue";
+export default defineComponent({
+  render() {
+    return (
+      <div>
+        <HelloWorld name="name" bigName="bigbig" />
+      </div>
+    )
+  }
+});
+</script>
---
+++ ohagi-front/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup lang="tsx">
+interface Props {
+  name: string;
+  bigName: string;
+}
+const props = defineProps<Props>();
+</script>
+
+<script lang="tsx">
+export default defineComponent({
+  render() {
+    return (
+      <div>
+        Hello {this.props.name}! {this.bigName}
+      </div>
+    );
+  },
+});
+</script>
